4c5eb480631ca4cea0d3c500dc5a74c04381e08c
BONNIN <francois.bonnin@ynov.com>
2022-01-18 02:22:30 +0100

    Add Routes

---
+++ database/migrations/2022_01_17_233947_create_choices_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('choices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('questions_id');
+
+            $table->string('label');
+            $table->boolean('answer');
+            $table->integer('earning');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('choices');
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/refresh', [AuthController::class, 'refresh']);
+    Route::get('/user-profile', [AuthController::class, 'userProfile']);
+});
+
+Route::get('/quiz/{id}');
+Route::post('/quiz', quiz);
+Route::put('/quiz/{quizId}', quiz);
+Route::delete('/quiz/{quiz.id}');
+Route::post('/quiz/{quiz.id}/publish');
+Route::post('/quiz/{quiz.id}/unpublish');
+Route::get('/quiz/{quizId}/questions');
+
+Route::get('/question/{questionId}/choices');
+
+Route::post('/score', { answers, quiz_id: quizId });
+Route::get('/score');
+
+Route::get('/user/' + userId);
